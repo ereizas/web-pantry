815c8946587cf5f9204b89cca1bed641c254e25f
Eric Reizas <tuo17224@temple.edu>
2024-07-29 19:17:11 -0400

    Added list of items

--- app/page.js
+++ app/page.js
@@ -1,5 +1,35 @@
-
+import {Box, Stack, Typography} from '@mui/material'
+const item = ['onion','tomato','lettuce','cucumber','garlic','ginger','carrot']
 export default function Home() {
-  return (
+  return (<Box
+    width="100vw" 
+    height="100vh"
+    display={"flex"}
+    justifyContent={"center"}
+    flexDirection={'column'}
+    alignItems={"center"}
+    >
+      <Stack width="800px"height="300px"spacing={2} overflow={'auto'}>
+      {item.map((i)=>(
+        <Box
+          key={i}
+          width="100%"
+          height="300px"
+          display={'flex'}
+          justifyContent={'center'}
+          alignItems={'center'}
+          bgcolor={'f0f0f0'}
+        >
+          <Typography
+            variant={'h3'}
+            color={'#333'}
+            textAlign={'center'}
+          >
+            {i.charAt(0).toUpperCase() + i.slice(1)}
+          </Typography>
+        </Box>
+      ))}
+      </Stack>
+  </Box>
   );
 }
