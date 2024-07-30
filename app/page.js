42de144cb04a17a1d6becbe7fd11b8ff5fc09301
Eric Reizas <tuo17224@temple.edu>
2024-07-30 16:45:31 -0400

    Created modal for adding items

--- app/page.js
+++ app/page.js
@@ -1,24 +1,54 @@
 'use client'
-import {Box, Stack, Typography} from '@mui/material'
+import {Box, Stack, Typography, Button, Modal, TextField} from '@mui/material'
 import { useEffect, useState } from 'react'
 import { firestore } from '@/firebase'
 import {collection, query, where, getDocs} from 'firebase/firestore'
 
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  pt: 2,
+  px: 4,
+  pb: 3,
+  gap: 3,
+  display: 'flex',
+  flexDirection: 'column'
+};
+
 export default function Home() {
   const [pantry, setPantry] = useState([])
+
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const [itemName, setItemName] = useState('')
+  
+  const updatePantry = async () => {
+    const snapshot = query(collection(firestore,'pantry'))
+    const docs = await getDocs(snapshot)
+    const pantryList = []
+    docs.forEach((doc) => {
+      pantryList.push(doc.id)
+    })
+    console.log(pantryList)
+    setPantry(pantryList)
+  }
+
   useEffect( ()=> {
-    const updatePantry = async () => {
-      const snapshot = query(collection(firestore,'pantry'))
-      const docs = await getDocs(snapshot)
-      const pantryList = []
-      docs.forEach((doc) => {
-        pantryList.push(doc.id)
-      })
-      console.log(pantryList)
-      setPantry(pantryList)
-    }
     updatePantry()
   },[])
+
+  const addItem = (item) =>{
+    console.log(item)
+  }
+
   return (<Box
     width="100vw" 
     height="100vh"
@@ -26,7 +56,38 @@ export default function Home() {
     justifyContent={"center"}
     flexDirection={'column'}
     alignItems={"center"}
+    gap={2}
     >
+      <Modal
+      open={open}
+        onClose={handleClose}
+        aria-labelledby="item-adder"
+        aria-describedby="item-adder-description"
+      >
+        <Box sx={style}>
+          <Typography id="item-adder" variant="h6" component="h2">
+            Add Item
+          </Typography>
+          <Stack width ="100%" direction={'row'} spacing={2}>
+            <TextField
+            id="outlined-basic"
+            label="Item"
+            variant="outlined"
+            fullWidth 
+            value={itemName}
+            onChange={(e) => setItemName(e.target.value)}/>
+            <Button variant="outlined" 
+            onClick={()=>{
+              addItem(itemName)
+              setItemName('')
+              handleClose()
+            }}>
+              Add
+            </Button>
+          </Stack>
+        </Box>
+      </Modal>
+      <Button variant="contained" onClick={handleOpen}>Add</Button>
       <Box border={'1px solid #333'}>
         <Box 
         width="800px"
